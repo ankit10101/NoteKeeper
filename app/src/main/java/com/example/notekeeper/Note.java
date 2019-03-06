7a64df59a92093849df2ff819c858bcc4597a467
Ankit Bajaj <ankitbajaj333@gmail.com>
2019-03-06 23:15:42 +0530

    Added Splash Screen

--- app/src/main/java/com/example/notekeeper/Note.java
+++ app/src/main/java/com/example/notekeeper/Note.java
@@ -1,11 +1,5 @@
 package com.example.notekeeper;
 
-// Known as Model Class
-
-import android.arch.persistence.room.ColumnInfo;
-import android.arch.persistence.room.Entity;
-import android.arch.persistence.room.Ignore;
-import android.arch.persistence.room.PrimaryKey;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 import android.support.annotation.ColorRes;
